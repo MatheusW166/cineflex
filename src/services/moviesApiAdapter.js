0b74c8efcffa22e5a32bc0e96122e957ab6078e8
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-03-08 17:50:41 -0300

    Missing 'awaits' fixed

--- src/services/moviesApiAdapter.js
+++ src/services/moviesApiAdapter.js
@@ -4,19 +4,19 @@ const BASE_URL = "https://mock-api.driven.com.br/api/v8/cineflex";
 
 const moviesApiAdapter = {
   getMovies: async () => {
-    const res = axios.get(`${BASE_URL}/movies`);
+    const res = await axios.get(`${BASE_URL}/movies`);
     return res.data;
   },
   getMovieSessions: async (movieId) => {
-    const res = axios.get(`${BASE_URL}/movies/${movieId}/showtimes`);
+    const res = await axios.get(`${BASE_URL}/movies/${movieId}/showtimes`);
     return res.data;
   },
   getSessionSeats: async (sessionId) => {
-    const res = axios.get(`${BASE_URL}/showtimes/${sessionId}/seats`);
+    const res = await axios.get(`${BASE_URL}/showtimes/${sessionId}/seats`);
     return res.data;
   },
   reserveSeats: async (reservation) => {
-    const res = axios.post(`${BASE_URL}/seats/book-many`, reservation);
+    const res = await axios.post(`${BASE_URL}/seats/book-many`, reservation);
     return res.data;
   },
 };
